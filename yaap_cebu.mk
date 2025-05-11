9092d8055131586791dc0c4b189f52a917dbdb4a
4mede <4mede@tutamail.com>
2025-05-11 17:31:30 -0400

    cebu: commonize some inclusions
    Change-Id: Ic52c4fa0f921e89e075b5d0ba9102b8858f0b60a

--- yaap_cebu.mk
+++ yaap_cebu.mk
@@ -14,9 +14,6 @@ $(call inherit-product, device/motorola/cebu/device.mk)
  $(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
-# Inherit from ViPER4AndroidFX
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := yaap_cebu
 PRODUCT_DEVICE := cebu
